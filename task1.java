c2523de4234aec685f5cd7f00aed2bec6fd53fa0
Oxygen <grandnorik@mail.ru>
2023-03-21 12:44:54 +0600

    task1 done

--- task1.java
+++ task1.java
@@ -1,5 +1,26 @@
 //Пусть дан произвольный список целых чисел, удалить из него чётные числа
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class task1 {
- 
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> intlist = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int value = rnd.nextInt(100);
+            intlist.add(value);
+        }
+        System.out.println(intlist);
+        int i = 0;
+        while (i < intlist.size()) {
+            if (intlist.get(i) % 2 == 0) {
+                intlist.remove(i);
+            }
+            else {
+                i++;
+            }
+        }
+        System.out.println(intlist);
+    }
 }
