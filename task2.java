0fc66709dfaf09ee5a7ebad1f7e61ce5de16d6e7
Oxygen <grandnorik@mail.ru>
2023-03-22 10:49:20 +0600

    proc

--- task2.java
+++ task2.java
@@ -16,23 +16,23 @@ public class task2 {
         System.out.println(intlist);
         int max = Collections.max(intlist);
         int min = Collections.min(intlist);
-        int maxItem = intlist.get(0);
-        int minItem = intlist.get(0);
+        // int maxItem = intlist.get(0);
+        // int minItem = intlist.get(0);
         int sumItems = 0;
         for (int item : intlist) {
-            if (item > maxItem) {
-                maxItem = item;
-            }
-            if (item < minItem) {
-                minItem = item;
-            }
+            // if (item > maxItem) {
+            //     maxItem = item;
+            // }
+            // if (item < minItem) {
+            //     minItem = item;
+            // }
             sumItems += item;
         }
         float average = (float)sumItems/intlist.size();
         System.out.printf("Максимальный элемент при помощи max %s\n", max);
-        System.out.printf("Максимальный элемент %s\n", maxItem);
+        //System.out.printf("Максимальный элемент %s\n", maxItem);
         System.out.printf("Минимальный элемент при помощи min %s\n", min);
-        System.out.printf("Минимальный элемент %s\n", minItem);
+        //System.out.printf("Минимальный элемент %s\n", minItem);
         System.out.printf("Сумма элементов %s\n", sumItems);
         System.out.printf("Среднее арифметическое %s\n", average);
     }
